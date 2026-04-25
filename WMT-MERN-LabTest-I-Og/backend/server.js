e74ef6b30eba9852e64d1914b310142dc70dc28f
IT24102730 <it24102730@my.sliit.lk>
2026-04-25 11:12:15 +0530

    Remove deprecated mongoose connect options

--- WMT-MERN-LabTest-I-Og/backend/server.js
+++ WMT-MERN-LabTest-I-Og/backend/server.js
@@ -16,10 +16,7 @@ app.use('/api/items', itemsRouter);
 
 // Database Connection
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => {
     console.log('Connected to MongoDB Atlas');
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
